547d85d6582e3949ad338b0758b2ea9db9cf9fe9
jmatorrales <96matorrales@gmail.com>
2024-09-19 13:07:56 +0200

    update web, new version and clear files

--- js/skills.js
+++ js/skills.js
@@ -1,87 +1,38 @@
-// canvas de idiomas
-const idm = document.getElementById('idiomas').getContext('2d');
-const frnt = document.getElementById("front").getContext('2d');
+// canvas de backend
 const bck = document.getElementById("back").getContext('2d');
 
 // Datos para el grafico
-
-// Idiomas
-const data_idm = {
-    labels : ['Castellano', 'Catalan', 'Ingles', 'Japones', 'Portugues'],
-    datasets: [{
-        label: 'Idiomas',
-        data: [10, 10, 6, 3, 2],
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-        borderWidth: 1
-    }]
-};
-
-// Front-end
-const data_frnt = {
-    labels : ['HTML/CSS', 'JavaScript', 'React'],
-    datasets: [{
-        label: 'Frontend',
-        data: [7, 5, 0],
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-        borderWidth: 1
-    }]
-};
-
 // Back-end
 const data_bck = {
-    labels : ['C#', 'Java', 'Python', 'Kotlin', 'SQL', 'Kusto'],
+    labels : ['C#', 'Java', 'Python', 'Kotlin', 'SQL / NoSQL', 'Kusto'],
     datasets: [{
         label: 'Backend',
         data: [7, 7, 6, 7, 8, 7],
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgba(54, 162, 235, 1)',
+        backgroundColor: 'rgba(118,250,118,0.2)',
+        borderColor: 'rgb(143,188,143,1)',
         borderWidth: 1
     }]
-};
-
-// Configuración grafico
-const config_idm = {
-    type: 'radar',
-    data: data_idm,
-    options: {
-        scale: {
-            r: {
-                suggestedMin: 0,
-                suggestedMax: 10,
-                ticks: {
-                    beginAtZero: true,
-                    stepSize: 2
-                }
-            }
-        }
-    }
-};
-
-const config_frnt = {
-    type: 'radar',
-    data: data_frnt,
-    options: {
-        scale: {
-            r: {
-                suggestedMin: 0,
-                suggestedMax: 10,
-                ticks: {
-                    beginAtZero: true,
-                    stepSize: 2
-                }
-            }
-        }
-    }
 };
 
 const config_bck = {
     type: 'radar',
     data: data_bck,
     options: {
+        plugins: {
+            legend: {
+                display: false  // Ocultar la leyenda
+            }
+        },
         scale: {
             r: {
+                pointLabels: {
+                    font: {
+                        size: '100' , // Ajusta el tamaño de la letra de los labels
+                        // family: 'Arial', // Cambia el tipo de letra
+                        // style: 'italic', // Cambia el estilo de la fuente
+                        weight: 'bold'  // Cambia el grosor de la fuente
+                    }
+                },
                 suggestedMin: 0,
                 suggestedMax: 10,
                 ticks: {
@@ -94,6 +45,4 @@ const config_bck = {
 };
 
 // Inicializar grafico
-new Chart(idm, config_idm);
-new Chart(frnt, config_frnt);
 new Chart(bck, config_bck);
